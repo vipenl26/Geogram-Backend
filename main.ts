7d8757fdd8ea28aa77643e776ea02a240bcb28b1
vipenl26 <vipenl26@gmail.com>
2023-04-29 23:29:31 +0530

    url update for cors dependency

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import "https://deno.land/x/dotenv@v3.2.2/load.ts"
 import server from "./graphql/server.ts";
 import createTAblesInDb from "./database_service/createTablesInDB.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 const PORT = Number(Deno.env.get("PORT")) || 3000;
 
