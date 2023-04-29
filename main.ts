438f421c474a04270d1a2f510474c0922c13373a
vipenl26 <vipenl26@gmail.com>
2023-04-29 23:29:04 +0530

    added cors

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import "https://deno.land/x/dotenv@v3.2.2/load.ts"
 import server from "./graphql/server.ts";
 import createTAblesInDb from "./database_service/createTablesInDB.ts";
-
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const PORT = Number(Deno.env.get("PORT")) || 3000;
 
 await createTAblesInDb()
+server.use(oakCors())
 
 server.listen({port: PORT})
