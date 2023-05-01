849a9e69a0bb46bbe1c6e3c62e3116c078710e08
vipenl26 <vipenl26@gmail.com>
2023-05-01 18:12:19 +0530

    updated getAllFriends to return full profile

--- service/getAllFriends.ts
+++ service/getAllFriends.ts
@@ -1,12 +1,22 @@
 
 import getEntriesFromFriendsTable from "../database_service/getEntriesFromFriendsTable.ts"
+import getProfile from "./getProfile.ts"
 import { getUserIdFromContext } from "./helper.ts"
 
 
 const getAllFriends = async(ctx:Record<string, unknown>, limit: number, offset: number) => {
     const id = getUserIdFromContext(ctx)
 
-    return await getEntriesFromFriendsTable(id, limit, offset)
+    const ls =  await getEntriesFromFriendsTable(id, limit, offset)
+
+    const res = []
+    for (const x in ls) {
+        const obj = await getProfile(ctx, ls[x])
+        res.push({...obj, id: ls[x]})
+    }
+
+    return res
+
 }
 
 export default getAllFriends
